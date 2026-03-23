4cbcb1d9c38b3abb885ced69d72d975be24c4dd6
Robert Levin <r.levin@student.neoversity.com.ua>
2026-03-23 22:36:27 +0100

    Initial commit: Flask homework project

---
+++ app.py
@@ -0,0 +1,62 @@
+﻿from flask import Flask, request, render_template, send_from_directory
+import json
+from datetime import datetime
+import os
+
+app = Flask(__name__, template_folder='.', static_folder='.')
+
+DATA_FILE = "storage/data.json"
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/message", methods=["GET", "POST"])
+def message():
+    if request.method == "GET":
+        return render_template("message.html")
+
+    raw_data = request.get_data().decode()
+    parts = raw_data.split("&")
+    data = {}
+
+    for p in parts:
+        key, value = p.split("=")
+        data[key] = value.replace("+", " ")
+
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r", encoding="utf-8") as f:
+            all_messages = json.load(f)
+    else:
+        all_messages = {}
+
+    timestamp = str(datetime.now())
+    all_messages[timestamp] = data
+
+    # Записуємо назад
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(all_messages, f, indent=2)
+
+    return "Message saved!"
+
+
+@app.route("/read")
+def read():
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
+        messages = json.load(f)
+    return render_template("read.html", messages=messages)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("error.html"), 404
+
+
+@app.route("/static/<path:path>")
+def static_files(path):
+    return send_from_directory(".", path)
+
+
+app.run(port=3000)
